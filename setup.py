8edb949ca87ca461a74aea359343a017ae50ccc7
Anthony Alteirac <anthony@alteirac.com>
2023-08-10 22:01:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="streamlit-highcharts",
-    version="0.1.6",
+    version="0.1.7",
     author="Anthony Alteirac",
     author_email="anthony@alteirac.com",
     description="Simple wrapper for Highcharts JS libs",
